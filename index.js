01584e9aba130f519a846d59bc92c571c31a0776
Kaviyarasan Thirunavukkarasu <kthirunavukkarasu@tmcmf.com>
2024-12-11 18:15:25 +0530

    fix: port changed and worlds spelling corrected

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = 3002;
 
 // Simple route
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('Hello, Worldssss!');
 });
 
 // Start server
